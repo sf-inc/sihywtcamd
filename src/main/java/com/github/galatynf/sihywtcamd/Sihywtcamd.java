3bce6a7f401ae0e8e74ccc2d6e4cc68cd2844bce
Galatynf <galvana.ciaran@outlook.fr>
2020-10-29 19:51:36 +0100

    Init

---
+++ src/main/java/com/github/galatynf/sihywtcamd/Sihywtcamd.java
@@ -0,0 +1,10 @@
+package com.github.galatynf.sihywtcamd;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sihywtcamd implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/galatynf/sihywtcamd/client/SihywtcamdClient.java
@@ -0,0 +1,13 @@
+package com.github.galatynf.sihywtcamd.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SihywtcamdClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
