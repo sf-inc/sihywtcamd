8626ded843d9091bcdaa533a7d86a41ea5566e3e
GalatynF <62654368+GalatynF@users.noreply.github.com>
2021-02-17 19:53:17 +0100

    Update ZombieMixin.java

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/ZombieMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/ZombieMixin.java
@@ -1,7 +1,9 @@
 package com.github.galatynf.sihywtcamd.mixin;
 
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
@@ -9,19 +11,21 @@ import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ZombieEntity.class)
-public abstract class ZombieMixin extends LivingEntity {
+@Mixin(LivingEntity.class)
+public abstract class ZombieMixin extends Entity{
 
-    protected ZombieMixin(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
+    public ZombieMixin(EntityType<?> type, World world) {
+        super(type, world);
     }
 
     @ModifyArg(method = "damage", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/entity/LivingEntity;applyDamage(Lnet/minecraft/entity/damage/DamageSource;F)V"))
     private float reduceDamage(DamageSource source, float amount) {
-        if(source.getAttacker() instanceof LivingEntity) {
+        if(((source.getAttacker()) instanceof LivingEntity) && this.getType().equals(EntityType.ZOMBIE)) {
             LivingEntity atker = (LivingEntity)source.getAttacker();
             if(EnchantmentHelper.getLevel(Enchantments.SMITE, atker.getMainHandStack()) > 0)
                 return amount;
