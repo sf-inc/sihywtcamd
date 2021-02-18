146991db838485ebe0406932e11802d1b357c98d
Charly <charly5bollinger@gmail.com>
2021-02-18 18:12:36 +0100

    Drowned moves faster

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/DrownedMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/DrownedMixin.java
@@ -2,16 +2,21 @@ package com.github.galatynf.sihywtcamd.mixin;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.mob.DrownedEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(DrownedEntity.class)
-public class DrownedMixin extends ZombieEntity {
+public abstract class DrownedMixin extends ZombieEntity {
+    @Shadow protected abstract boolean isTargetingUnderwater();
+
     public DrownedMixin(EntityType<? extends ZombieEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -23,6 +28,17 @@ public class DrownedMixin extends ZombieEntity {
             this.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.TRIDENT));
         } else if (rand < 18 ) {
             this.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.FISHING_ROD));
+        }
+    }
+
+    @Override
+    public void travel(Vec3d movementInput) {
+        if (this.canMoveVoluntarily() && this.isTouchingWater() && this.isTargetingUnderwater()) {
+            this.updateVelocity(0.1F, movementInput);
+            this.move(MovementType.SELF, this.getVelocity());
+            this.setVelocity(this.getVelocity().multiply(0.9D));
+        } else {
+            super.travel(movementInput);
         }
     }
 }
