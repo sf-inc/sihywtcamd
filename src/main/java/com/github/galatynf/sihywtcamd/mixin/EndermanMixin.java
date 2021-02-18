80c24f9a14536961d5d611f98f0dca4528417f8e
Charly <charly5bollinger@gmail.com>
2021-02-19 00:36:03 +0100

    Looking at enderman gives blindness
    When you look at en enderman, you get blind for 5 seconds. You should really not stop watching it because then it will teleports to you and you won't see it coming

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/EndermanMixin.java
@@ -0,0 +1,33 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EndermanEntity.class)
+public class EndermanMixin extends HostileEntity {
+    protected EndermanMixin(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "isPlayerStaring", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getRotationVec(F)Lnet/minecraft/util/math/Vec3d;"))
+    private void addBlindness(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+        Vec3d vec3d = player.getRotationVec(1.0F).normalize();
+        Vec3d vec3d2 = new Vec3d(this.getX() - player.getX(), this.getEyeY() - player.getEyeY(), this.getZ() - player.getZ());
+        double d = vec3d2.length();
+        vec3d2 = vec3d2.normalize();
+        double e = vec3d.dotProduct(vec3d2);
+        if (e > 1.0D - 0.025D / d && player.canSee(this)) {
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 100));
+        }
+    }
+}
