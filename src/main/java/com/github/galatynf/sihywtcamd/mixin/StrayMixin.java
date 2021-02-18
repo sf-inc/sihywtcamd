0edbf565d6373b12e558468df7ccb7b37fde2c55
Charly <charly5bollinger@gmail.com>
2021-02-18 18:35:18 +0100

    Stray's arrow has slowness 2
    If you're hit by an arrow from a stray, it will give you slowness 2 instead of 1 (not really impressive but better)

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/StrayMixin.java
@@ -0,0 +1,21 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.StrayEntity;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(StrayEntity.class)
+public class StrayMixin {
+    @ModifyVariable(method = "createArrowProjectile", at = @At("TAIL"))
+    private PersistentProjectileEntity arrowSlownessIncreased(PersistentProjectileEntity projectileEntity) {
+        if (projectileEntity instanceof ArrowEntity) {
+            ((ArrowEntity) projectileEntity).addEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 600, 1));
+        }
+        return projectileEntity;
+    }
+}
