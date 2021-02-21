f28dc75f836800404450ebb6a011e6e2d2a3b288
Charly <charly5bollinger@gmail.com>
2021-02-21 11:37:58 +0100

    Blindness on shulker bullet hit
    Add blindness if player. Also rename the blindness method of endermen to have no name conflict

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/ShulkerBulletMixin.java
@@ -0,0 +1,21 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ShulkerBulletEntity;
+import net.minecraft.util.hit.EntityHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ShulkerBulletEntity.class)
+public class ShulkerBulletMixin {
+    @Inject(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;)Z"))
+    private void addBlindnessS(EntityHitResult entityHitResult, CallbackInfo ci) {
+        if (entityHitResult.getEntity() instanceof PlayerEntity) {
+            ((PlayerEntity) entityHitResult.getEntity()).addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 50));
+        }
+    }
+}
