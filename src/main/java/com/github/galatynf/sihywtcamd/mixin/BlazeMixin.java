333289be924a936257247feb314ff502b4b03d8f
Galatynf <galvana.ciaran@outlook.fr>
2021-02-21 20:00:13 +0100

    Added Blaze mixin
    Not even tested lol

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/BlazeMixin.java
@@ -0,0 +1,28 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.BlazeEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class BlazeMixin extends Entity{
+
+    public BlazeMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(at=@At("HEAD"), method="damage")
+    private void setFire(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        Entity atker = source.getAttacker();
+        if(atker instanceof BlazeEntity && !source.isProjectile()) {
+            this.setOnFireFor(5);
+        }
+    }
+}
--- src/main/java/com/github/galatynf/sihywtcamd/mixin/SkeletonMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/SkeletonMixin.java
@@ -19,6 +19,6 @@ public abstract class SkeletonMixin extends HostileEntity {
 
     @Inject(at=@At("HEAD"), method = "initGoals")
     private void fleePlayer(CallbackInfo ci) {
-        this.goalSelector.add(3, new FleeEntityGoal<>(this, PlayerEntity.class, 2, 1.0, 1.2, (livingEntity) -> (this.getMainHandStack().getItem() == Items.BOW)));
+        this.goalSelector.add(3, new FleeEntityGoal<>(this, PlayerEntity.class, 2, 1.5, 1.7, (livingEntity) -> (this.getMainHandStack().getItem() == Items.BOW)));
     }
 }
