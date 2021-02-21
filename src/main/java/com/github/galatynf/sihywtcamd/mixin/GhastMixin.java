86d541321035d1607e83411d7b0241e6adf5db0a
Charly <charly5bollinger@gmail.com>
2021-02-21 12:15:21 +0100

    Double health for ghast

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/GhastMixin.java
@@ -0,0 +1,20 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.GhastEntity;
+import net.minecraft.entity.mob.MobEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(GhastEntity.class)
+public class GhastMixin {
+    @Inject(method = "createGhastAttributes", at = @At("HEAD"), cancellable = true)
+    private static void changeHealth(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
+        cir.setReturnValue(MobEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 20.0D)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 100.0D));
+    }
+}
