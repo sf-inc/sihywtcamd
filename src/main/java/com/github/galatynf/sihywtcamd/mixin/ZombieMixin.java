343e9cfc1f239f7c486124725f8f01e7eec4cb55
Charly <charly5bollinger@gmail.com>
2021-02-21 16:18:24 +0100

    Fixed damage zombies
    The new default damage for zombies was affecting every living entity because was badly coded

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/ZombieMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/ZombieMixin.java
@@ -1,22 +1,18 @@
 package com.github.galatynf.sihywtcamd.mixin;
 
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public abstract class ZombieMixin extends Entity{
+public abstract class ZombieMixin extends Entity {
 
     public ZombieMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -25,16 +21,12 @@ public abstract class ZombieMixin extends Entity{
     @ModifyArg(method = "damage", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/entity/LivingEntity;applyDamage(Lnet/minecraft/entity/damage/DamageSource;F)V"))
     private float reduceDamage(DamageSource source, float amount) {
-        if(((source.getAttacker()) instanceof LivingEntity) && this.getType().equals(EntityType.ZOMBIE)) {
-            LivingEntity atker = (LivingEntity)source.getAttacker();
-            if(EnchantmentHelper.getLevel(Enchantments.SMITE, atker.getMainHandStack()) > 0)
-                return amount;
-            }
-        if(source.isOutOfWorld()
-            || source.isFire()
-            || source.isSourceCreativePlayer()) {
-            return amount;
+        Entity attacker = source.getAttacker();
+        if (this.getType().equals(EntityType.ZOMBIE)
+                && attacker instanceof LivingEntity
+                && EnchantmentHelper.getLevel(Enchantments.SMITE, ((LivingEntity) attacker).getMainHandStack()) < 1) {
+            return 1;
         }
-        return 1;
+        return amount;
     }
 }
