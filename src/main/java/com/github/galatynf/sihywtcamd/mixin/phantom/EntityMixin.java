ef0a02725c5fdd7c69e650e841fc4e0f460c633d
Charly <charly5bollinger@gmail.com>
2021-03-23 16:42:29 +0100

    Phantoms don't collide with blocks

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/phantom/EntityMixin.java
@@ -0,0 +1,22 @@
+package com.github.galatynf.sihywtcamd.mixin.phantom;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Shadow public abstract EntityType<?> getType();
+
+    @Inject(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At("HEAD"), cancellable = true)
+    private void noCollisionForPhantom(Vec3d movement, CallbackInfoReturnable<Vec3d> cir) {
+        if (this.getType().equals(EntityType.PHANTOM)) {
+            cir.setReturnValue(movement);
+        }
+    }
+}
