be4fbce4aa1d4ed852b70043056c76f7a3d60d09
Charly <charly5bollinger@gmail.com>
2021-03-25 19:10:31 +0100

    Witches spawn a bit more ofter

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/BiomeFeatMixin.java
@@ -0,0 +1,19 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DefaultBiomeFeatures.class)
+public class BiomeFeatMixin {
+    @Inject(method = "addMonsters", at = @At("TAIL"))
+    private static void changeWitchSpawnrate(SpawnSettings.Builder builder, int zombieWeight, int zombieVillagerWeight,
+                                             int skeletonWeight, CallbackInfo ci) {
+        builder.spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.WITCH, 3, 1, 1));
+    }
+}
