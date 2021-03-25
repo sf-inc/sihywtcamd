e6c8ae83abb02338825884b0db6613733cf5bac6
Charly <charly5bollinger@gmail.com>
2021-03-25 19:39:13 +0100

    Witches flee players

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/WitchMixin.java
@@ -0,0 +1,24 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.FleeEntityGoal;
+import net.minecraft.entity.mob.WitchEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.raid.RaiderEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WitchEntity.class)
+public abstract class WitchMixin extends RaiderEntity {
+    protected WitchMixin(EntityType<? extends RaiderEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(at=@At("HEAD"), method = "initGoals")
+    private void fleePlayer(CallbackInfo ci) {
+        this.goalSelector.add(1, new FleeEntityGoal<>(this, PlayerEntity.class, 3, 1.2, 1.5, (livingEntity) -> true));
+    }
+}
