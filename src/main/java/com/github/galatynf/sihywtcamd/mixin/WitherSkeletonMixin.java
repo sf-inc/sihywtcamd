e7b3d830a6fc5838552b3c81fc8a45fb6677a6c1
Charly <charly5bollinger@gmail.com>
2021-03-26 20:50:53 +0100

    Wither skeletons can spawn baby
    Baby chance 1/5

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/WitherSkeletonMixin.java
@@ -0,0 +1,74 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.*;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.SpiderEntity;
+import net.minecraft.entity.mob.WitherSkeletonEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WitherSkeletonEntity.class)
+public class WitherSkeletonMixin extends HostileEntity {
+    private static final TrackedData<Boolean> BABY = DataTracker.registerData(SpiderEntity .class, TrackedDataHandlerRegistry.BOOLEAN);
+
+    protected WitherSkeletonMixin(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "initialize", at = @At("TAIL"))
+    private void canSpawnBaby(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData,
+                              CompoundTag entityTag, CallbackInfoReturnable<EntityData> cir) {
+        this.setBaby(this.random.nextFloat() < 0.2F);
+    }
+
+    @Override
+    public void initDataTracker() {
+        super.initDataTracker();
+        this.getDataTracker().startTracking(BABY, false);
+    }
+
+    @Override
+    public boolean isBaby() {
+        return this.getDataTracker().get(BABY);
+    }
+
+    @Override
+    public void setBaby(boolean baby) {
+        this.getDataTracker().set(BABY, baby);
+    }
+
+    @Override
+    public void onTrackedDataSet(TrackedData<?> data) {
+        if (BABY.equals(data)) {
+            this.calculateDimensions();
+        }
+
+        super.onTrackedDataSet(data);
+    }
+
+    @Override
+    public void readCustomDataFromTag(CompoundTag tag) {
+        super.readCustomDataFromTag(tag);
+        this.setBaby(tag.getBoolean("IsBaby"));
+    }
+
+    @Override
+    public void writeCustomDataToTag(CompoundTag tag) {
+        super.writeCustomDataToTag(tag);
+        tag.putBoolean("IsBaby", this.isBaby());
+    }
+
+    @Override
+    public float getActiveEyeHeight(EntityPose pose, EntityDimensions dimensions) {
+        return 2.1F * this.getScaleFactor();
+    }
+}
