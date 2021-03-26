adb7365a40706f8572c47343e780a98896cc8d3f
Charly <charly5bollinger@gmail.com>
2021-03-26 22:30:36 +0100

    Evokers are invulnerable to projectile

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/EvokerMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/EvokerMixin.java
@@ -1,21 +1,34 @@
 package com.github.galatynf.sihywtcamd.mixin;
 
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.EvokerEntity;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.SpellcastingIllagerEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EvokerEntity.class)
-public class EvokerMixin {
+public abstract class EvokerMixin extends SpellcastingIllagerEntity {
+    protected EvokerMixin(EntityType<? extends SpellcastingIllagerEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
     @Inject(method = "createEvokerAttributes", at = @At("HEAD"), cancellable = true)
     private static void changeHealthE(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
         cir.setReturnValue(HostileEntity.createHostileAttributes()
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5D)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 12.0D)
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 36.0D));
+    }
+
+    @Override
+    public boolean isInvulnerableTo(DamageSource damageSource) {
+        return damageSource.isProjectile() || super.isInvulnerableTo(damageSource);
     }
 }
