4a03ba0382b78183143036e8d2f13feec622aa87
Charly <charly5bollinger@gmail.com>
2021-03-27 23:49:19 +0100

    Vindicators can spawn with a speed bonus
    1/5 spawn with a bonus speed from 0 to 42% depending on the local difficulty. That means with a low local difficulty they won't spawn with bonus, but with high local difficulty they can spawn with a speed bonus of 42%

---
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/VindicatorMixin.java
@@ -0,0 +1,37 @@
+package com.github.galatynf.sihywtcamd.mixin;
+
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.IllagerEntity;
+import net.minecraft.entity.mob.VindicatorEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
+
+@Mixin(VindicatorEntity.class)
+public abstract class VindicatorMixin extends IllagerEntity {
+    protected VindicatorMixin(EntityType<? extends IllagerEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "initialize", at = @At("HEAD"))
+    private void addSpeedBonusV(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData,
+                                CompoundTag entityTag, CallbackInfoReturnable<EntityData> cir) {
+        if (world.getRandom().nextFloat() < 0.2F) {
+            EntityAttributeInstance speed = this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+            Objects.requireNonNull(speed).addPersistentModifier(new EntityAttributeModifier(
+                    "Random vindicator bonus", 0.42 * difficulty.getClampedLocalDifficulty() * speed.getValue(), EntityAttributeModifier.Operation.ADDITION));
+        }
+    }
+}
