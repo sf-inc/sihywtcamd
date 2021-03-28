c32f54861ada7211cbb0ab22a62233ce39d2ff59
Charly <charly5bollinger@gmail.com>
2021-03-28 16:04:43 +0200

    Pillagers have enchanted crossbow more often
    2 times more often, but still depending on the local difficulty

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/PillagerMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/PillagerMixin.java
@@ -1,7 +1,9 @@
 package com.github.galatynf.sihywtcamd.mixin;
 
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
@@ -32,6 +34,13 @@ public abstract class PillagerMixin extends IllagerEntity {
             EntityAttributeInstance speed = this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
             Objects.requireNonNull(speed).addPersistentModifier(new EntityAttributeModifier(
                     "Random pillager bonus", 0.69 * difficulty.getClampedLocalDifficulty() * speed.getValue(), EntityAttributeModifier.Operation.ADDITION));
+        }
+    }
+
+    @Override
+    public void method_30759(float f) {
+        if (!this.getMainHandStack().isEmpty() && this.random.nextFloat() < 0.5F * f) {
+            this.equipStack(EquipmentSlot.MAINHAND, EnchantmentHelper.enchant(this.random, this.getMainHandStack(), (int)(5.0F + f * (float)this.random.nextInt(18)), false));
         }
     }
 }
