37db0141f9977b0525bbb9e97598c8ec8f19725c
Charly <charly5bollinger@gmail.com>
2021-03-29 21:25:06 +0200

    Slimes fuse on collision
    Slimes of the same size can fuse when colliding each other to create a bigger slime (only for 1 and 2 sizes, so 4 can't become rare 8)

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/SlimeMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/SlimeMixin.java
@@ -3,6 +3,7 @@ package com.github.galatynf.sihywtcamd.mixin;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.SlimeEntity;
 import net.minecraft.nbt.CompoundTag;
@@ -12,9 +13,14 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SlimeEntity.class)
 public abstract class SlimeMixin extends MobEntity {
+    @Shadow public abstract int getSize();
+
     @Shadow protected abstract void setSize(int size, boolean heal);
 
     protected SlimeMixin(EntityType<? extends MobEntity> entityType, World world) {
@@ -31,5 +37,21 @@ public abstract class SlimeMixin extends MobEntity {
         int j = 1 << i;
         this.setSize(j, true);
         return super.initialize(world, difficulty, spawnReason, entityData, entityTag);
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void tryToFuse(CallbackInfo ci) {
+        if (!this.world.isClient
+                && this.isAlive()
+                && this.getSize() < 4
+                && (this.world.getTime() % 5) == 0) {
+            SlimeEntity slimeEntity = this.world.getClosestEntity(SlimeEntity.class, TargetPredicate.DEFAULT, this,
+                    this.getX(), this.getY(), this.getZ(), this.getBoundingBox());
+            if (slimeEntity != null
+                    && this.getSize() == slimeEntity.getSize()) {
+                slimeEntity.remove();
+                this.setSize(this.getSize() * 2, true);
+            }
+        }
     }
 }
