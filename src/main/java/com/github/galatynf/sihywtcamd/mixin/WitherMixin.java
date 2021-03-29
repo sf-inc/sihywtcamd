e6d45af6dbcd03537c866cd75ffd793035fcbab7
Charly <charly5bollinger@gmail.com>
2021-03-29 23:00:30 +0200

    Withers summon wither skeletons when half health
    Just like in bedrock, they will spawn 3 to 4 wither skeletons when they are half health if the difficulty is normal or hard. If they gain health and have more than half health again, they won't spawn wither skeletons again

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/WitherMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/WitherMixin.java
@@ -1,16 +1,35 @@
 package com.github.galatynf.sihywtcamd.mixin;
 
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(WitherEntity.class)
-public class WitherMixin {
+public abstract class WitherMixin extends HostileEntity {
+    @Shadow public abstract boolean shouldRenderOverlay();
+
+    @Shadow public abstract int getInvulnerableTimer();
+
+    @Unique
+    private boolean sihywtcamd_hasSpawned = false;
+
+    protected WitherMixin(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
     @Inject(method = "createWitherAttributes", at = @At("HEAD"), cancellable = true)
     private static void changeHealth(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
         cir.setReturnValue(HostileEntity.createHostileAttributes()
@@ -18,5 +37,25 @@ public class WitherMixin {
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.6000000238418579D)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 40.0D)
                 .add(EntityAttributes.GENERIC_ARMOR, 4.0D));
+    }
+
+    @Inject(method = "mobTick", at = @At("HEAD"))
+    private void spawnWitherSkeletons(CallbackInfo ci) {
+        if (!this.world.isClient
+                && (this.world.getDifficulty().equals(Difficulty.NORMAL) || this.world.getDifficulty().equals(Difficulty.HARD))
+                && this.getInvulnerableTimer() < 1
+                && !sihywtcamd_hasSpawned
+                && this.shouldRenderOverlay()) {
+            int witherSummoned = 3;
+            if (this.random.nextFloat() < this.world.getLocalDifficulty(this.getBlockPos()).getClampedLocalDifficulty()) {
+                ++witherSummoned;
+            }
+            for (int i=0; i < witherSummoned; i++) {
+                EntityType.WITHER_SKELETON.spawn((ServerWorld) this.world, null, null, null, this.getBlockPos(),
+                        SpawnReason.EVENT, false, false);
+            }
+
+            sihywtcamd_hasSpawned = true;
+        }
     }
 }
