a4230b274b5827ae13c2a7acb9db2885161ed399
Charly <charly5bollinger@gmail.com>
2021-07-24 15:21:21 +0200

    Changed the light level requirement for cave spider spawn
    Now can't spawn if > 7 (was > 8, which is different from other mobs)

--- src/main/java/com/github/galatynf/sihywtcamd/Utils.java
+++ src/main/java/com/github/galatynf/sihywtcamd/Utils.java
@@ -15,7 +15,7 @@ public class Utils {
     public static boolean canCaveSpiderSpawn(EntityType<? extends HostileEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
         if (pos.getY() >= world.getSeaLevel()) {
             return false;
-        } else if (world.getLightLevel(pos) > 8) {
+        } else if (world.getLightLevel(pos) > 7) {
             return false;
         } else {
             return random.nextInt((int) Math.cbrt(pos.getY())) == 0 && canMobSpawn(type, world, spawnReason, pos, random);
