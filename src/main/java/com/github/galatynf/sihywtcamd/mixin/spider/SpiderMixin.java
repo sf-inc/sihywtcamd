db5f697679d5b1545caa85b4188660d49fb06e84
Charly <charly5bollinger@gmail.com>
2021-08-31 18:42:11 +0200

    Cobweb goal less accurate #19
    It was the same accuracy as skeletons so a bit too much. Not they are still quite accurate but they fail more often

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/spider/SpiderMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/spider/SpiderMixin.java
@@ -126,7 +126,7 @@ public class SpiderMixin extends HostileEntity implements RangedAttackMob {
         double e = target.getBodyY(0.3333333333333333D) - this.getY();
         double f = target.getZ() - this.getZ();
         double g = Math.sqrt(d * d + f * f);
-        cobwebProjectileEntity.setVelocity(d, e + g * 0.20000000298023224D, f, 1.0F, (float) (14 - this.world.getDifficulty().getId() * 4));
+        cobwebProjectileEntity.setVelocity(d, e + g * 0.20000000298023224D, f, 1.0F, (float) (14 - this.world.getDifficulty().getId() * 2));
         this.playSound(SoundEvents.ENTITY_SKELETON_SHOOT, 1.0F, 1.0F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
         this.world.spawnEntity(cobwebProjectileEntity);
     }
