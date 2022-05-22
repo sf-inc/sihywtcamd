8b99654972df2578a17bb703e8e5a4712df31221
charlyb01 <charly5bollinger@gmail.com>
2022-05-22 14:49:22 +0200

    Refacto skeletons in a new package

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/skeleton/StrayMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/skeleton/StrayMixin.java
@@ -1,4 +1,4 @@
-package com.github.galatynf.sihywtcamd.mixin;
+package com.github.galatynf.sihywtcamd.mixin.skeleton;
 
 import com.github.galatynf.sihywtcamd.config.ModConfig;
 import net.minecraft.entity.effect.StatusEffectInstance;
