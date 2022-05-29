dc2c9dd45507b4eed2e45859215947096c2163ff
charlyb01 <charly5bollinger@gmail.com>
2022-05-29 16:20:53 +0200

    Blaze sets on fire only if fire active
    Also rewrite mixin to mixin into blaze directly, and don't set on fire if is blocking with shield

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/BlazeMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/BlazeMixin.java
@@ -4,27 +4,27 @@ import com.github.galatynf.sihywtcamd.config.ModConfig;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.BlazeEntity;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(LivingEntity.class)
-public abstract class BlazeMixin extends Entity {
-    public BlazeMixin(EntityType<?> type, World world) {
-        super(type, world);
+@Mixin(BlazeEntity.class)
+public abstract class BlazeMixin extends HostileEntity {
+    @Shadow protected abstract boolean isFireActive();
+
+    protected BlazeMixin(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
     }
 
-    @Inject(method="damage", at = @At("HEAD"))
-    private void setOnFireOnCollision(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        Entity attacker = source.getAttacker();
-        if (ModConfig.get().nether.blazeFireCollision
-                && attacker instanceof BlazeEntity
-                && !source.isProjectile()) {
-            this.setOnFireFor(5);
+    @Override
+    public boolean tryAttack(Entity target) {
+        boolean success = super.tryAttack(target);
+        if (ModConfig.get().nether.blazeFireCollision && success
+                && this.isFireActive() && !((LivingEntity) target).isBlocking()) {
+            target.setOnFireFor(5);
         }
+        return success;
     }
 }
