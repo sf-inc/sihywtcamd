c317618a9e61933445801c2ee151b7a27d07a4df
charlyb01 <charly5bollinger@gmail.com>
2023-11-07 23:59:50 +0100

    Cleaner phantom swoop goal mixin
    Make it more understandable

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/phantom/PhantomSwoopGoalMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/phantom/PhantomSwoopGoalMixin.java
@@ -1,10 +1,8 @@
 package com.github.galatynf.sihywtcamd.mixin.phantom;
 
 import com.github.galatynf.sihywtcamd.config.ModConfig;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.entity.passive.CatEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.predicate.entity.EntityPredicates;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,33 +16,28 @@ import java.util.List;
 @Mixin(targets = "net.minecraft.entity.mob.PhantomEntity$SwoopMovementGoal")
 public abstract class PhantomSwoopGoalMixin {
     @Final @Shadow PhantomEntity field_7333;
-
-    @Shadow public abstract boolean canStart();
+    @Shadow private boolean catsNearby;
 
     @Inject(method = "shouldContinue()Z", at = @At("HEAD"), cancellable = true)
-    private void changePredicateContinue(CallbackInfoReturnable<Boolean> cir) {
-        if (ModConfig.get().overworld.general.mobsLessFear || ModConfig.get().overworld.phantom.lightFear) {
+    private void fearLight(CallbackInfoReturnable<Boolean> cir) {
+        // Reset to ensure further check validity
+        this.catsNearby = false;
+
+        if (ModConfig.get().overworld.phantom.lightFear
+                && this.field_7333.getWorld().getLightLevel(this.field_7333.getBlockPos()) > 10) {
+            cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "shouldContinue()Z", at = @At("TAIL"), cancellable = true)
+    private void removeCatFear(CallbackInfoReturnable<Boolean> cir) {
+        if (ModConfig.get().overworld.general.mobsLessFear && this.catsNearby) {
             boolean shouldContinue = true;
-            LivingEntity livingEntity = field_7333.getTarget();
-            if (livingEntity == null) {
-                shouldContinue = false;
-            } else if (!livingEntity.isAlive()) {
-                shouldContinue = false;
-            } else if (livingEntity instanceof PlayerEntity && (livingEntity.isSpectator() || ((PlayerEntity) livingEntity).isCreative())) {
-                shouldContinue = false;
-            } else if (!this.canStart()) {
-                shouldContinue = false;
-            } else if (ModConfig.get().overworld.phantom.lightFear && field_7333.getWorld().getLightLevel(field_7333.getBlockPos()) > 10) {
-                shouldContinue = false;
-            } else {
-                if (field_7333.age % 20 == 0) {
-                    List<CatEntity> list = field_7333.getWorld().getEntitiesByClass(CatEntity.class, field_7333.getBoundingBox().expand(16.0D), EntityPredicates.VALID_ENTITY);
-                    for (CatEntity catEntity : list) {
-                        catEntity.hiss();
-                        if (!(ModConfig.get().overworld.general.mobsLessFear && catEntity.getHealth() <= catEntity.getMaxHealth() / 2.0F)) {
-                            shouldContinue = false;
-                        }
-                    }
+            List<CatEntity> list = this.field_7333.getWorld().getEntitiesByClass(CatEntity.class, this.field_7333.getBoundingBox().expand(16.0), EntityPredicates.VALID_ENTITY);
+            for (CatEntity catEntity : list) {
+                if (catEntity.getHealth() > catEntity.getMaxHealth() / 2.0F) {
+                    shouldContinue = false;
+                    break;
                 }
             }
 
