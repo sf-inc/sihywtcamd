65f23d09698bcbabe83aea8eeb389fe2c7c66d77
charlyb01 <charly5bollinger@gmail.com>
2023-11-07 17:56:10 +0100

    Update cave spider spawn condition
    The deeper, the more common they spawn. They won't spawn above first third of height limit (ie. above 64).

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/spider/HostileMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/spider/HostileMixin.java
@@ -1,6 +1,5 @@
 package com.github.galatynf.sihywtcamd.mixin.spider;
 
-import com.github.galatynf.sihywtcamd.Utils;
 import com.github.galatynf.sihywtcamd.config.ModConfig;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
@@ -8,10 +7,14 @@ import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.minecraft.entity.mob.MobEntity.canMobSpawn;
 
 @Mixin(HostileEntity.class)
 public class HostileMixin {
@@ -21,7 +24,14 @@ public class HostileMixin {
         if (ModConfig.get().arthropod.spider.caveSpiderNaturalSpawn
                 && type.equals(EntityType.CAVE_SPIDER)
                 && !spawnReason.equals(SpawnReason.SPAWNER)) {
-            cir.setReturnValue(Utils.canCaveSpiderSpawn(type, world, spawnReason, pos, random));
+            cir.setReturnValue(canCaveSpiderSpawn(type, world, spawnReason, pos, random));
         }
+    }
+
+    @Unique
+    private static boolean canCaveSpiderSpawn(EntityType<? extends HostileEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+        double relativeHeight = (double) (pos.getY() - world.getBottomY()) / world.getHeight();
+        return random.nextDouble() < (1.F - 3.F * relativeHeight)
+                && canMobSpawn(type, world, spawnReason, pos, random);
     }
 }
