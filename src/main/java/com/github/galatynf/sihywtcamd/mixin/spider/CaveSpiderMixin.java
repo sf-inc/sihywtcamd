07639ddc72d9c0d889a1e4f723efcb72400bf8bd
charlyb01 <charly5bollinger@gmail.com>
2023-11-08 14:55:04 +0100

    Baby spiders give poison for less time than adults
    The duration of the poison is reduced by 5, falling from 7s/15s (normal/hard) to 1.4s/3s

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/spider/CaveSpiderMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/spider/CaveSpiderMixin.java
@@ -2,6 +2,8 @@ package com.github.galatynf.sihywtcamd.mixin.spider;
 
 import com.github.galatynf.sihywtcamd.config.ModConfig;
 import net.minecraft.entity.*;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.CaveSpiderEntity;
 import net.minecraft.entity.mob.SkeletonEntity;
 import net.minecraft.entity.mob.SpiderEntity;
@@ -13,6 +15,7 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CaveSpiderEntity.class)
@@ -47,5 +50,11 @@ public class CaveSpiderMixin extends SpiderEntity {
     @Inject(method = "getActiveEyeHeight", at = @At("HEAD"), cancellable = true)
     private void updateCaveSpiderEyeHeight(EntityPose pose, EntityDimensions dimensions, CallbackInfoReturnable<Float> cir) {
         cir.setReturnValue(0.45F * this.getScaleFactor());
+    }
+
+    @Redirect(method = "tryAttack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;Lnet/minecraft/entity/Entity;)Z"))
+    private boolean adaptivePoison(LivingEntity target, StatusEffectInstance effect, Entity source) {
+        int duration = this.isBaby() ? effect.getDuration() / 5 : effect.getDuration();
+        return target.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, duration, 0), this);
     }
 }
