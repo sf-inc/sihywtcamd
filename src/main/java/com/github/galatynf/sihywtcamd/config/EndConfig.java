735b12f49bddeeb5c3cb91af6ec6b1ff26540410
charlyb01 <charly5bollinger@gmail.com>
2023-11-13 22:53:47 +0100

    Endermites teleport target on attack
    It tries to teleport the target on attack, a similar way to chorus fruits

--- src/main/java/com/github/galatynf/sihywtcamd/config/EndConfig.java
+++ src/main/java/com/github/galatynf/sihywtcamd/config/EndConfig.java
@@ -2,10 +2,18 @@ package com.github.galatynf.sihywtcamd.config;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "end")
 public class EndConfig implements ConfigData {
     public boolean endermanBlindness = true;
     public boolean shulkerCancelLevitation = true;
     public boolean endermiteInChorus = true;
+
+    @ConfigEntry.Gui.CollapsibleObject
+    public Endermite endermite = new Endermite();
+
+    public static class Endermite {
+        public boolean teleportAttack = true;
+    }
 }
--- src/main/java/com/github/galatynf/sihywtcamd/mixin/EndermiteMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/EndermiteMixin.java
@@ -1,18 +1,21 @@
 package com.github.galatynf.sihywtcamd.mixin;
 
 import com.github.galatynf.sihywtcamd.config.ModConfig;
-import net.minecraft.entity.EntityData;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.*;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.EndermiteEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -22,6 +25,30 @@ import java.util.Objects;
 public class EndermiteMixin extends HostileEntity {
     protected EndermiteMixin(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    public boolean tryAttack(Entity entity) {
+        boolean bl = super.tryAttack(entity);
+        if (bl && ModConfig.get().end.endermite.teleportAttack
+                && entity instanceof LivingEntity target) {
+            BlockPos oldPos = target.getBlockPos();
+            for (BlockPos pos : BlockPos.iterateRandomly(this.random, 8, oldPos, 8)) {
+                if (target.hasVehicle()) {
+                    target.stopRiding();
+                }
+                if (!target.teleport(pos.getX(), pos.getY(), pos.getZ(), true)) {
+                    continue;
+                }
+
+                this.getWorld().emitGameEvent(GameEvent.TELEPORT, oldPos, GameEvent.Emitter.of(target));
+                SoundEvent soundEvent = SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT;
+                this.getWorld().playSound(null, oldPos.getX(), oldPos.getY(), oldPos.getZ(), soundEvent, SoundCategory.PLAYERS, 1.0f, 1.0f);
+                target.playSound(soundEvent, 1.0f, 1.0f);
+                break;
+            }
+        }
+        return bl;
     }
 
     @Nullable
