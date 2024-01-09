683718851967c62d89ccfa8fa037033e14dae725
charlyb01 <charly5bollinger@gmail.com>
2024-01-08 19:10:58 -0500

    Add towering baby zombies advancements
    Add two advancements to look baby tower through a spyglass

--- src/main/java/com/github/galatynf/sihywtcamd/data/DataGenerator.java
+++ src/main/java/com/github/galatynf/sihywtcamd/data/DataGenerator.java
@@ -5,12 +5,15 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
-import net.minecraft.advancement.Advancement;
-import net.minecraft.advancement.AdvancementEntry;
-import net.minecraft.advancement.AdvancementFrame;
-import net.minecraft.advancement.AdvancementRequirements;
+import net.minecraft.advancement.*;
 import net.minecraft.advancement.criterion.OnKilledCriterion;
+import net.minecraft.advancement.criterion.UsingItemCriterion;
 import net.minecraft.item.Items;
+import net.minecraft.predicate.entity.EntityFlagsPredicate;
+import net.minecraft.predicate.entity.EntityPredicate;
+import net.minecraft.predicate.entity.PlayerPredicate;
+import net.minecraft.predicate.item.ItemPredicate;
+import net.minecraft.registry.tag.EntityTypeTags;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -48,6 +51,68 @@ public class DataGenerator implements DataGeneratorEntrypoint {
                     .criterion("killed_by_something", OnKilledCriterion.Conditions.createEntityKilledPlayer())
                     .requirements(AdvancementRequirements.anyOf(List.of("kill_something", "killed_by_something")))
                     .build(consumer, Sihywtcamd.MOD_ID + "/root");
+
+            AdvancementEntry babyZombiesTower1 = Advancement.Builder.create()
+                    .parent(root)
+                    .display(
+                            Items.SPYGLASS,
+                            Text.translatable("advancements.spyglass_at_baby_1.title"),
+                            Text.translatable("advancements.spyglass_at_baby_1.description"),
+                            null,
+                            AdvancementFrame.TASK,
+                            true,
+                            true,
+                            false
+                    )
+                    .criterion("spyglass_at_baby_1", UsingItemCriterion.Conditions.create(
+                            EntityPredicate.Builder.create()
+                                    .typeSpecific(PlayerPredicate.Builder.create()
+                                            .lookingAt(EntityPredicate.Builder.create()
+                                                    .type(EntityTypeTags.ZOMBIES)
+                                                    .flags(EntityFlagsPredicate.Builder.create().isBaby(true))
+                                                    .passenger(EntityPredicate.Builder.create()
+                                                            .type(EntityTypeTags.ZOMBIES)
+                                                            .flags(EntityFlagsPredicate.Builder.create().isBaby(true))))
+                                            .build()),
+                            ItemPredicate.Builder.create().items(Items.SPYGLASS))
+                    )
+                    .build(consumer, Sihywtcamd.MOD_ID + "/spyglass_at_baby_1");
+
+            AdvancementEntry babyZombiesTower4 = Advancement.Builder.create()
+                    .parent(babyZombiesTower1)
+                    .display(
+                            Items.SPYGLASS,
+                            Text.translatable("advancements.spyglass_at_baby_4.title"),
+                            Text.translatable("advancements.spyglass_at_baby_4.description"),
+                            null,
+                            AdvancementFrame.GOAL,
+                            true,
+                            true,
+                            false
+                    )
+                    .criterion("spyglass_at_baby_4", UsingItemCriterion.Conditions.create(
+                            EntityPredicate.Builder.create()
+                                    .typeSpecific(PlayerPredicate.Builder.create()
+                                            .lookingAt(EntityPredicate.Builder.create()
+                                                    .type(EntityTypeTags.ZOMBIES)
+                                                    .flags(EntityFlagsPredicate.Builder.create().isBaby(true))
+                                                    .passenger(EntityPredicate.Builder.create()
+                                                            .type(EntityTypeTags.ZOMBIES)
+                                                            .flags(EntityFlagsPredicate.Builder.create().isBaby(true))
+                                                            .passenger(EntityPredicate.Builder.create()
+                                                                    .type(EntityTypeTags.ZOMBIES)
+                                                                    .flags(EntityFlagsPredicate.Builder.create().isBaby(true))
+                                                                    .passenger(EntityPredicate.Builder.create()
+                                                                            .type(EntityTypeTags.ZOMBIES)
+                                                                            .flags(EntityFlagsPredicate.Builder.create().isBaby(true))
+                                                                            .passenger(EntityPredicate.Builder.create()
+                                                                                    .type(EntityTypeTags.ZOMBIES)
+                                                                                    .flags(EntityFlagsPredicate.Builder.create().isBaby(true)))))))
+                                            .build()),
+                            ItemPredicate.Builder.create().items(Items.SPYGLASS))
+                    )
+                    .rewards(AdvancementRewards.Builder.experience(50))
+                    .build(consumer, Sihywtcamd.MOD_ID + "/spyglass_at_baby_4");
         }
     }
 }
