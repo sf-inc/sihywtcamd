8aaef50b2b0d46e41b5e3d341b73898b42359d63
charlyb01 <charly5bollinger@gmail.com>
2024-01-19 22:53:31 -0500

    Fix shulker bullets levitation check
    Check if the entity has the effect before applying it

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/ShulkerBulletMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/ShulkerBulletMixin.java
@@ -1,24 +1,24 @@
 package com.github.galatynf.sihywtcamd.mixin;
 
 import com.github.galatynf.sihywtcamd.config.ModConfig;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ShulkerBulletEntity;
-import net.minecraft.util.hit.EntityHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ShulkerBulletEntity.class)
 public class ShulkerBulletMixin {
-    @Inject(method = "onEntityHit", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/entity/LivingEntity;addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;Lnet/minecraft/entity/Entity;)Z"))
-    private void cancelLevitation(EntityHitResult entityHitResult, CallbackInfo ci) {
+    @Redirect(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;Lnet/minecraft/entity/Entity;)Z"))
+    private boolean cancelLevitation(LivingEntity livingEntity, StatusEffectInstance effect, Entity source) {
         if (ModConfig.get().end.shulker.cancelLevitation
-                && entityHitResult.getEntity() instanceof PlayerEntity playerEntity
-                && playerEntity.hasStatusEffect(StatusEffects.LEVITATION)
-                && playerEntity.getRandom().nextFloat() < 0.33f) {
-            playerEntity.removeStatusEffect(StatusEffects.LEVITATION);
+                && livingEntity.hasStatusEffect(StatusEffects.LEVITATION)
+                && livingEntity.getRandom().nextFloat() < 0.33f) {
+            return livingEntity.removeStatusEffect(StatusEffects.LEVITATION);
         }
+        return livingEntity.addStatusEffect(effect, source);
     }
 }
