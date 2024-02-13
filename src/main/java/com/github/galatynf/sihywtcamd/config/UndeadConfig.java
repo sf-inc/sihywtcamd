c37ddf507b147f53a6a2b6fbfe7f2535a3b65dd6
charlyb01 <charly5bollinger@gmail.com>
2024-02-13 10:51:18 -0500

    Undead ignore other undead attack

--- src/main/java/com/github/galatynf/sihywtcamd/config/UndeadConfig.java
+++ src/main/java/com/github/galatynf/sihywtcamd/config/UndeadConfig.java
@@ -8,6 +8,13 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 @Config(name = "undead")
 public class UndeadConfig implements ConfigData {
     @ConfigEntry.Gui.CollapsibleObject
+    public General general = new General();
+
+    public static class General {
+        public boolean ignoreUndeadAttacks = true;
+    }
+
+    @ConfigEntry.Gui.CollapsibleObject
     public Phantom phantom = new Phantom();
 
     public static class Phantom {
