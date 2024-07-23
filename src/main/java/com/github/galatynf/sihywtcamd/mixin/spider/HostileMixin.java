6510d6f5168342e8a3e6a97aec0cc6f502578cd6
charlyb01 <charly5bollinger@gmail.com>
2024-07-23 11:38:51 +0200

    Fix cave spider natural spawn not respecting light condition

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/spider/HostileMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/spider/HostileMixin.java
@@ -14,8 +14,6 @@ import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HostileEntity.class)
 public class HostileMixin extends PathAwareEntity {
@@ -23,21 +21,21 @@ public class HostileMixin extends PathAwareEntity {
         super(entityType, world);
     }
 
-    @Inject(method = "canSpawnInDark", at = @At("HEAD"), cancellable = true)
-    private static void changeCaveSpiderSpawn(EntityType<? extends HostileEntity> type, ServerWorldAccess world, SpawnReason spawnReason,
-                                              BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
-        if (ModConfig.get().arthropods.caveSpider.naturalSpawn
-                && type.equals(EntityType.CAVE_SPIDER)
-                && !spawnReason.equals(SpawnReason.SPAWNER)) {
-            cir.setReturnValue(canCaveSpiderSpawn(type, world, spawnReason, pos, random));
-        }
+    @ModifyReturnValue(method = "canSpawnInDark", at = @At("RETURN"))
+    private static boolean changeCaveSpiderSpawn(boolean original, EntityType<? extends HostileEntity> type,
+                                              ServerWorldAccess world, SpawnReason spawnReason, BlockPos pos,
+                                              Random random) {
+        return original &&
+                (!spawnReason.equals(SpawnReason.NATURAL)
+                        || !type.equals(EntityType.CAVE_SPIDER)
+                        || canCaveSpiderSpawn(world, pos, random));
     }
 
     @Unique
-    private static boolean canCaveSpiderSpawn(EntityType<? extends HostileEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+    private static boolean canCaveSpiderSpawn(WorldAccess world, BlockPos pos, Random random) {
         double relativeHeight = (double) (pos.getY() - world.getBottomY()) / world.getHeight();
-        return random.nextDouble() < (1.F - 3.F * relativeHeight)
-                && canMobSpawn(type, world, spawnReason, pos, random);
+        return ModConfig.get().arthropods.caveSpider.naturalSpawn
+                && random.nextDouble() < (1.F - 3.F * relativeHeight);
     }
 
     @ModifyReturnValue(method = "shouldDropLoot", at = @At("RETURN"))
