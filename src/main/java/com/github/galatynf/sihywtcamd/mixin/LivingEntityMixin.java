8d63668f3651d003a3c3fa48116b67550d1e20f9
HowardCLH <adamhnc@gmail.com>
2025-07-03 04:15:33 -0400

    Attempt to fix undead healing with dumb solution

--- src/main/java/com/github/galatynf/sihywtcamd/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/galatynf/sihywtcamd/mixin/LivingEntityMixin.java
@@ -19,6 +19,7 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -30,7 +31,8 @@ public abstract class LivingEntityMixin extends Entity {
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
-
+    @Unique public float xlastDamageTaken;
+    @Unique public float xtimeUntilRegen;
     @Shadow protected float lastDamageTaken;
     @Shadow public float bodyYaw;
 
@@ -46,12 +48,16 @@ public abstract class LivingEntityMixin extends Entity {
             cir.setReturnValue(0);
         }
     }
-
+    @Inject(method = "damage", at = @At("HEAD"))
+    private void copyValues(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir){
+        xlastDamageTaken = this.lastDamageTaken;
+        xtimeUntilRegen = this.timeUntilRegen;
+    }
     @Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;becomeAngry(Lnet/minecraft/entity/damage/DamageSource;)V"))
     private void healUndeadAttacker(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         if (ModConfig.get().undead.general.attackHeal && amount > 0) {
-            float realAmount = (float) this.timeUntilRegen > 10.0f && !source.isIn(DamageTypeTags.BYPASSES_COOLDOWN)
-                    ? amount - this.lastDamageTaken : amount;
+            float realAmount = this.xtimeUntilRegen > 10.0f && !source.isIn(DamageTypeTags.BYPASSES_COOLDOWN)
+                    ? amount - this.xlastDamageTaken : amount;
 
             LivingEntity attacker = source.getAttacker() instanceof LivingEntity
                     ? (LivingEntity) source.getAttacker()
